0080dd6d41a45994a1dbf67134353f2d8d65f7d2
JHong-Hsu <jhong.su@gmail.com>
2017-06-27 16:23:55 +0800

    update test_mlp.py

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -8,26 +8,24 @@ X = [[0., 0., 0.], [1., 1., 1.]]
 y = [0, 1]
 
 # sklearn MLPRegressor
-clf = MLPRegressor(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(5, 2), random_state=1,
-                    activation="softmax")
-
-# scikitlearn_plus MLPRegressor
-clf_plus = MLPRegressor_plus(solver='lbfgs', alpha=1e-5,
+clf = MLPRegressor(solver='sgd', alpha=1e-5,
                     hidden_layer_sizes=(5, 2), random_state=1)
 
+# scikitlearn_plus MLPRegressor
+clf_plus = MLPRegressor_plus(solver='sgd', alpha=1e-5,
+                    hidden_layer_sizes=(5, 2), random_state=1)
 
-
-clf.fit(X, y)   
+ 
 
 print("------sklearn------")
+clf.fit(X, y) 
 print(clf.predict([[2., 2., 2.], [-1., -2., -3.]]))
 print([coef.shape for coef in clf.coefs_])
 print()
 
-clf_plus.fit(X,y)
 
 print("------scikitlearn_plus------")
+clf_plus.fit(X,y)
 print(clf_plus.predict([[2., 2., 2.], [-1., -2., -3.]]))
 print([coef.shape for coef in clf_plus.coefs_])
 print()
